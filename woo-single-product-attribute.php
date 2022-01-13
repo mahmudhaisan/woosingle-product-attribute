df4b1da0cbeadb15f00872cda0f4e9ab77c2b979
mahmudhaisan <mahmudhaisan@gmail.com>
2022-01-13 17:45:41 +0600

    getting product parent category id

--- woo-single-product-attribute.php
+++ woo-single-product-attribute.php
@@ -15,11 +15,9 @@
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  */
 
-
 if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
-
 
 //adding stylesheets and scripts
 function woospa_scripts()
@@ -29,27 +27,33 @@ function woospa_scripts()
 }
 
 add_action('wp_enqueue_scripts', 'woospa_scripts');
-
-
-
 //woo attribut function
 function wooattr_add()
 {
     require_once('main.php');
     // declaring $product
     global $product;
-
     //get current category
     $current_categories = $product->get_categories();
     echo ($current_categories);
 
+    echo "<pre>";
+    $cats = get_the_terms($product->ID, 'product_cat');
+
+    //getting products parent category id
+    foreach ($cats as $cat) {
+        echo $cat->parent . ' ';
+        break;
+    }
+
+    print_r($cats);
+    echo "</pre>";
+
     // getting attributes and looping all available attribute
     $attributes = $product->get_attributes();
-
     //for each loop starts        
     echo '<div class="jumbotron">';
     echo '<div class="row w-100">';
-
 
     foreach ($attributes as $attribute) {
         $attribute_name = $attribute->get_taxonomy();
@@ -60,17 +64,7 @@ function wooattr_add()
         $attribute_name_get = ($attribute_data['name']);
         $attr_prefix_remove = explode('pa_', $attribute_name_get);
         $attr_after_explode = $attr_prefix_remove[1];
-
-        echo '<div class="col-md-3">';
-
-
-
-
-
-
-?>
-
-
+        echo '<div class="col-md-3">'; ?>
 
         <div class="card border-info mx-sm-1 p-3">
             <div class="text-info text-center mt-3">
@@ -86,21 +80,11 @@ function wooattr_add()
             </div>
         </div>
         </div>
-
-
-
     <?php
     } ?>
-
     </div>
     </div>
-
 <?php
-
 }
-
-
 add_action('woocommerce_after_single_product_summary', 'wooattr_add');
-
-
 ?>
